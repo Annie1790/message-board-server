3022098095151fc3cfbe1bd82ca954a6c9dc51b7
Annie1790 <budayaniko@outlook.com>
2023-05-22 17:52:36 +0100

    project done

--- server.js
+++ server.js
@@ -1,23 +1,49 @@
+//Require
 const express = require("express");
 const cors = require("cors");
-const app = express();
-const fs = require("fs");
-const port = 4000;
 const resolve = require("path").resolve;
-let db = fs.createWriteStream("database.json");
+const fs = require("fs");
 
+//Server
+const app = express();
+const port = 4000;
+
+//Variables
 app.use(express.json())
-
 app.use(cors());
 
+//Random color picker
+
+function randomClr() {
+    const hex = ["#FFADAD", "#FFD6A5", "#FDFFB6","#CAFFBF","#9BF6FF","#A0C4FF","#BDB2FF","#FFC6FF","#fec89a","#e3d5ca",];
+    const i = Math.floor(Math.random() * hex.length);
+    return hex[i];
+}
+
 app.get("/", (req, res) => {
-    res.sendFile(resolve("./database.json"))
+    fs.readFile(resolve("./database.json"), 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+            res.json([]);
+            res.status(404);
+        } else {
+            res.json(data
+                .split("\n")
+                .filter((segment) => segment != "")
+                .map((segment => JSON.parse(segment))));
+            res.status(200)
+        }
+    });
 })
 
 app.post("/", (req, res) => {
-    res.json("success");
-    db.write(JSON.stringify(req.body));
-    db.end();
+    try {
+        res.json("success");
+        req.body.color = randomClr();
+        fs.writeFileSync(resolve("./database.json"), JSON.stringify(req.body) + "\n", { flag: "a+" }, error => { console.log(error) });
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.listen(port, () => {
